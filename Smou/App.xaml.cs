d35550228cf855abfcd347bc1cb33f024ec5caf3
Alberto Suárez Cánovas <a.suarez@externo.ibermatica.com>
2023-06-08 15:31:21 +0200

    Agregar archivos de proyecto.

---
+++ Smou/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Smou;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Smou/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Smou;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
